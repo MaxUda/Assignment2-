1b41cb9d03113e5551548d52b60f3c542dc9c859
Leenk0 <132303487+Leenk0@users.noreply.github.com>
2024-06-08 15:44:46 +1200

    version 1.2

--- src/magicrpg/Help.java
+++ src/magicrpg/Help.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package magicrpg;
 
-/**
- *
- * @author gurle
- */
 public class Help {
     
 }
